a9575491d58cb6d1858c456e91baec18e6372d63
Apollo Tang <tang.apollo@gmail.com>
2019-08-03 00:07:31 -0400

    refactor upgrade-package.js to having either devDep or Dep being absense

--- upgrade-package.js
+++ upgrade-package.js
@@ -7,7 +7,7 @@
  * https://stackabuse.com/reading-and-writing-json-files-with-node-js/
  * https://medium.com/@the_teacher/yarn-upgrade-does-not-update-package-json-solution-9cd6122e6c6c
  * https://www.diigo.com/user/gofish?query=npm-update
- *
+ */
 
 'use strict';
 
@@ -16,33 +16,45 @@ const exec = require('child_process').exec;
 
 
 fs.readFile('package.json', (err, data) => {
-    if (err) throw err;
-    let packageContent = JSON.parse(data);
+  if (err) throw err;
+
+  let packageContent = JSON.parse(data);
+
+  let depComand = `echo 'no Dependencies'`
+  let devDepComand = `echo 'no devDependencies'`
+
+  if (packageContent && packageContent.devDependencies) {
     const devDependencies = Object.keys(packageContent.devDependencies)
+    if (devDependencies.length > 0) {
+      const devDependenciesString =  devDependencies.join(' ')
+      devDepComand = `yarn add -D ${devDependenciesString}`
+    }
+  }
+
+  if (packageContent && packageContent.dependencies) {
     const dependencies = Object.keys(packageContent.dependencies)
-    const devDependenciesString =  devDependencies.join(' ');
-    const dependenciesString =  dependencies.join(' ');
+    if (dependencies.length > 0) {
+      const dependenciesString =  dependencies.join(' ')
+      depComand = `yarn add ${dependenciesString}`
+    }
+  }
 
-    const devDepComand = `yarn add -D ${devDependenciesString}`
-    console.log(devDepComand);
-    exec(devDepComand,
-        (error, stdout, stderr) => {
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
+  console.log(`running command: ${devDepComand}`);
+  exec(devDepComand, (error, stdout, stderr) => {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    if (error !== null) {
+      console.log(`exec error: ${error}`)
+    }
 
-        const depComand = `yarn add ${dependenciesString}`
-
-        console.log(depComand);
-        exec(depComand ,
-            (error, stdout, stderr) => {
-                console.log(`stdout: ${stdout}`);
-                console.log(`stderr: ${stderr}`);
-                if (error !== null) {
-                    console.log(`exec error: ${error}`);
-                }
-        });
+    console.log(`running command: ${depComand}`)
+    exec(depComand ,
+      (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+        if (error !== null) {
+            console.log(`exec error: ${error}`)
+        }
     });
+  });
 });
