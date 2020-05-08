50082f2baadf6a11ba9e97b22b7e017728a4cbd1
Apollo Tang <tang.apollo@gmail.com>
2020-05-07 22:49:00 -0400

    add documentation

--- duplicate-exclude-git-node-modules.sh
+++ duplicate-exclude-git-node-modules.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-# This script clone a folder to specified destination,
-# it exclude node_modules/ and .git/
+#
+# This script take a folder path:
+#
+#   /path/to/folder
+#
+# and make a duplication of its content to a destination folder path:
+#
+#   /path/to/folder--duplicate
+#
+# The duplicated content excluded top level node_modules and .git
+#
+# Note that the trailing slash of the source folder should be specified.
+#
+#    wrong:   /path/to/folder/
+#    correct: /path/to/folder
+#
 
 echo $1
 if [ -z "$1" ]
@@ -20,4 +34,5 @@ echo " Destination        : $destinationName"
 
 rsync -havp $sourcefolder $destinationName --exclude node_modules --exclude .git
 # The excluded folder  is relative to the sourcefolder
-# if souce is a folder it must end with slas
+# if souce is a folder it must end with slash
+
