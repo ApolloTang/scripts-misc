7c73ec48f8663e0cbd389dca42b82f6feb5dfaea
Apollo Tang <tang.apollo@gmail.com>
2021-05-02 16:17:14 -0400

    add do for rm-node-modules-recursive.sh

--- rm-node-modules-recursive.sh
+++ rm-node-modules-recursive.sh
@@ -4,6 +4,10 @@
 #
 # This script remove all node_modules/ folder recusively in the pwd
 #
+# Todo:
+#   add a -n flag for dry run
+#   print out removing path
+#
 # -----------------------------------------------------------------
 
 find . -type d -name node_modules -prune -exec rm -rf {} \;
